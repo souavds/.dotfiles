8ea696de4ba4a48f8a1a711217949e8efa4373c9
Arthur Diniz <arthurvdinizs@gmail.com>
2023-12-17 14:37:17 -0300

    chore: init linking dotfiles

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,57 +3,147 @@
 cd "$(dirname "$0")"
 export DOTFILES=$(pwd -P)
 
-COMMAND=""
+SYMBOL="\uf444"
 
-while getopts c: flag
-do
-    case "${flag}" in
-        c) COMMAND=${OPTARG};;
-    esac
-done
+info () {
+  printf "\033[00;34m $SYMBOL $1 \033[0m\n"
+}
 
-if [$COMMAND == ""]
-then
-  echo "You need to pass the install command flag (-c 'sudo zypper in -y')"
+user () {
+  printf "\033[0;33m $SYMBOL $1 \033[0m\n"
+}
+
+success () {
+  printf "\033[00;32m $SYMBOL $1 \033[0m\n"
+}
+
+fail () {
+  printf "\033[0;31m $SYMBOL $1 \033[0m\n"
+  echo ''
   exit
-fi
+}
+
+command=
+
+function get_install_command() {
+  user "What is the install command in your system? (i.e. brew install)"
+  read -p "   " command < /dev/tty
+}
 
 function install_shell() {
-  echo "Installing fish shell"
+  info "Installing fish shell"
   
-  $COMMAND fish
+  $command fish
 
   read -p "Do you want to make fish the default shell? (y/n) " yn < /dev/tty
 
   case $yn in
     [Yy]* ) chsh -s $(which fish) ;;
-    [Nn]* ) echo "> Skipped"; return ;;
-    * ) echo "> Incorrect option, skipping"; return ;;
+    [Nn]* ) info "> Skipped" ;;
+    * ) info "> Incorrect option, skipping";;
   esac
 
-  echo "Finished installing shell"
+  success "Installed fish shell"
 }
 
 function install_packages() {
-  echo "Installing packages"
+  info "Installing packages"
 
   find -H "$DOTFILES" -maxdepth 2 -name 'packages' | while read packagesfile
   do
     cat "$packagesfile" | while read line
     do
-      echo "Installing package -> $line"
-      $COMMAND $line
+      info "Installing package: $line"
+      $command $line
+      success "Installed package: $line"
     done
   done
      
-  echo "Finished installing packages"
+  success "Installed packages"
+}
+
+function link_file() {
+  local src=$1 dst=$2
+
+  local skip=
+  local overwrite=
+  local backup=
+  local action=
+
+  if [ -f "$dst" ] || [ -d "$dst" ] || [ -L "$dst" ]
+  then
+
+    local first_line="Path to $dst already exists, what do you want to do?"
+    local second_line="[o]verwrite, [s]kip, [S]kip all, [b]ackup?"
+    read -p "$first_line $second_line " yn < /dev/tty
+
+    case $yn in
+      [S]* ) echo "> Skipped symlinking all duplicated files"; break ;;
+      [s]* ) echo "> Skipped symlinking $(basename "$src")"; skip=true ;;
+      [o]* ) overwrite=true ;;
+      [b]* ) backup=true ;;
+      * ) echo "> Incorrect option, skipping"; skip=true ;;
+    esac
+
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      echo "> Removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      local timestamp = $(date +%s)
+      local backup = "${dst}.backup($timestamp)"
+      mv "$dst" "$backup"
+      echo "> Moved $dst to $backup"
+    fi
+
+  fi
+
+  if [ "$skip" != "true" ]
+  then
+    ln -s "$src" "$dst"
+    echo "> Linked $1 to $2"
+  fi
+
+  echo ""
+}
+
+function install_dotfiles() {
+  info "Installing dotfiles"
+
+  find -H "$DOTFILES" -maxdepth 2 -name 'linking' | while read linkfile
+  do
+    cat "$linkfile" | while read line
+    do
+        local src dst dir
+        src=$(eval echo "$line" | cut -d '=' -f 1)
+        dst=$(eval echo "$line" | cut -d '=' -f 2)
+        dir=$(dirname $dst)
+
+        mkdir -p "$dir"
+        link_file "$src" "$dst"
+    done
+  done
+
+  success "Installed dotfiles"
+}
+
+function install_asdf() {
+  info "Installing asdf"
+
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions;
 }
 
 
-echo "Initiating setup..."
+info "Initiating setup..."
+get_install_command
 install_shell
-echo ""
 install_packages
-echo ""
+install_dotfiles
+success "Finished setup"
 
 
