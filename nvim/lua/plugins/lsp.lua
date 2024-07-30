fece6202dd3ba4e1bf0202bbf0e4ff151fa15b75
Arthur Diniz <arthurvdinizs@gmail.com>
2024-07-29 21:10:10 -0300

    chore: remove lsp-zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,108 +1,167 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    -- LSP Support
-    { 'neovim/nvim-lspconfig' },
-    {
-      'williamboman/mason.nvim',
-      build = function()
-        pcall(vim.api.nvim_command, 'MasonUpdate')
-      end
-    },
-    { 'williamboman/mason-lspconfig.nvim' },
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{ "folke/lazydev.nvim" },
+		{ "williamboman/mason.nvim", config = true },
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lua' },
-    {
-      "zbirenbaum/copilot-cmp",
-      config = function()
-        require("copilot_cmp").setup()
-      end
-    },
+		{ "j-hui/fidget.nvim", opts = {} },
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+		{ "Bilal2453/luvit-meta", lazy = true },
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' },
-    { 'rafamadriz/friendly-snippets' },
+		-- Autoformatting
+		"stevearc/conform.nvim",
+	},
+	config = function()
+		local capabilities = nil
+		if pcall(require, "cmp_nvim_lsp") then
+			capabilities = require("cmp_nvim_lsp").default_capabilities()
+		end
 
-    -- Kinds
-    { 'onsails/lspkind.nvim' }
-  },
-  config = function()
-    -- LSP
-    local lsp = require('lsp-zero')
+		local lspconfig = require("lspconfig")
 
-    lsp.on_attach(function(client, bufnr)
-      lsp.default_keymaps({ buffer = bufnr })
+		local servers = {
+			lua_ls = {
+				server_capabilities = {
+					semanticTokensProvider = vim.NIL,
+				},
+			},
 
-      -- autoformat
-      lsp.buffer_autoformat()
-    end)
+			tsserver = {
+				server_capabilities = {
+					documentFormattingProvider = false,
+				},
+			},
 
-    require('mason').setup({})
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        'rust_analyzer',
-        'gopls',
-        'html',
-        'lua_ls',
-        'tailwindcss',
-        'elixirls'
-      },
-      handlers = {
-        function(server_name)
-          require('lspconfig')[server_name].setup({})
-        end,
-        lua_ls = function()
-          local lua_opts = lsp.nvim_lua_ls()
-          require('lspconfig').lua_ls.setup(lua_opts)
-        end
-      }
-    })
+			lexical = {
+				filetypes = { "elixir", "eelixir", "heex" },
+				cmd = { "/home/avds/.lsp/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+				root_dir = require("lspconfig.util").root_pattern({ "mix.exs" }),
+				server_capabilities = {
+					completionProvider = vim.NIL,
+					definitionProvider = false,
+				},
+			},
 
-    lsp.set_sign_icons({
-      error = '✘',
-      warn = '▲',
-      hint = '⚑',
-      info = '»'
-    })
+			gopls = {
+				settings = {
+					gopls = {
+						hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							functionTypeParameters = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						},
+					},
+				},
+			},
 
-    -- CMP
-    local cmp = require('cmp')
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			cssls = {},
+		}
 
-    cmp.setup({
-      setup = {
-        { name = 'copilot' },
-        { name = 'nvim_lsp' },
-        { name = 'buffer' },
-        { name = 'path' },
-        { name = 'nvim_lua' }
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      formatting = {
-        fields = { 'abbr', 'kind', 'menu' },
-        format = require('lspkind').cmp_format({
-          mode = 'symbol',
-          maxwidth = 50,
-          ellipsis_char = '...',
-          symbol_map = { Copilot = "" },
-        })
-      },
-      mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
-      }),
-    })
-  end
+		local servers_to_install = vim.tbl_filter(function(key)
+			local t = servers[key]
+			if type(t) == "table" then
+				return not t.manual_install
+			else
+				return t
+			end
+		end, vim.tbl_keys(servers))
+
+		require("mason").setup()
+		local ensure_installed = {
+			"stylua",
+			"lua_ls",
+		}
+
+		vim.list_extend(ensure_installed, servers_to_install)
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+		for name, config in pairs(servers) do
+			if config == true then
+				config = {}
+			end
+			config = vim.tbl_deep_extend("force", {}, {
+				capabilities = capabilities,
+			}, config)
+
+			lspconfig[name].setup(config)
+		end
+
+		local disable_semantic_tokens = {
+			lua = true,
+		}
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local bufnr = args.buf
+				local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+				local settings = servers[client.name]
+				if type(settings) ~= "table" then
+					settings = {}
+				end
+
+				local builtin = require("telescope.builtin")
+
+				vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+				vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+				vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+				vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+				vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+
+				vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+				vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+
+				local filetype = vim.bo[bufnr].filetype
+				if disable_semantic_tokens[filetype] then
+					client.server_capabilities.semanticTokensProvider = nil
+				end
+
+				-- Override server capabilities
+				if settings.server_capabilities then
+					for k, v in pairs(settings.server_capabilities) do
+						if v == vim.NIL then
+							---@diagnostic disable-next-line: cast-local-type
+							v = nil
+						end
+
+						client.server_capabilities[k] = v
+					end
+				end
+			end,
+		})
+
+		-- Autoformatting setup
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			callback = function(args)
+				require("conform").format({
+					bufnr = args.buf,
+					lsp_fallback = true,
+					quiet = true,
+				})
+			end,
+		})
+	end,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,38 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "TSUpdateSync" },
-  config = function()
-    require 'nvim-treesitter.configs'.setup {
-      ensure_installed = {
-        "html",
-        "css",
-        "gitcommit",
-        "json",
-        "json5",
-        "lua",
-        "markdown",
-        "vim",
-        "rust",
-        "go",
-        "elixir"
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true
-      }
-    }
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "TSUpdateSync" },
+	opts = {
+		ensure_installed = {
+			"bash",
+			"diff",
+			"html",
+			"css",
+			"gitcommit",
+			"json",
+			"json5",
+			"lua",
+			"luadoc",
+			"markdown",
+			"vim",
+			"rust",
+			"go",
+			"elixir",
+		},
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.install").prefer_git = true
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
