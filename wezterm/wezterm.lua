4d9452e5ddeda423fcc10b7597852514a6b5e238
Arthur Diniz <arthurvdinizs@gmail.com>
2024-07-30 19:10:38 -0300

    chore: change wezterm active tab keys

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,24 @@
-local w = require('wezterm')
-
+local w = require("wezterm")
+local act = w.action
 local config = {}
 
-if w.config_builder then config = w.config_builder() end
+if w.config_builder then
+	config = w.config_builder()
+end
 
 -- system
-config.enable_wayland = false
+config.enable_wayland = true
 
 -- window
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
 
 -- color
-config.color_scheme = 'GruvboxDark'
+config.color_scheme = "GruvboxDark"
 
 -- font
 config.font = w.font_with_fallback({
-  'GeistMono Nerd Font',
+	"GeistMono Nerd Font",
 })
 config.font_size = 16
 
@@ -25,53 +27,61 @@ config.font_size = 16
 
 -- nvim navigator
 local function is_vim(pane)
-  return pane:get_user_vars().IS_NVIM == 'true'
+	return pane:get_user_vars().IS_NVIM == "true"
 end
 
 local direction_keys = {
-  Left = 'h',
-  Down = 'j',
-  Up = 'k',
-  Right = 'l',
-  -- reverse lookup
-  h = 'Left',
-  j = 'Down',
-  k = 'Up',
-  l = 'Right',
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
 }
 
 local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
-    action = w.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
-        }, pane)
-      else
-        if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = w.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
 end
 
 config.keys = {
-  -- move between split panes
-  split_nav('move', 'h'),
-  split_nav('move', 'j'),
-  split_nav('move', 'k'),
-  split_nav('move', 'l'),
-  -- resize panes
-  split_nav('resize', 'h'),
-  split_nav('resize', 'j'),
-  split_nav('resize', 'k'),
-  split_nav('resize', 'l'),
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 }
+
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL",
+		action = act.ActivateTab(i - 1),
+	})
+end
 
 return config
