0c1f79d5c9c9f5d6b571a49a6ce351120c979206
Arthur Diniz <arthurvdinizs@gmail.com>
2024-10-04 22:23:29 -0300

    chore: switch from mini plugins

--- .config/nvim/lua/plugins/mini/notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "echasnovski/mini.notify",
-  version = false,
-  config = function()
-    require("mini.notify").setup()
-    vim.notify = require("mini.notify").make_notify()
-  end,
-}
