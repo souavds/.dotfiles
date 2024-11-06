6c62e5a601984afd1046c4f3d6e7cdfd0f9c2c65
Arthur Diniz <arthurvdinizs@gmail.com>
2024-11-05 22:27:43 -0300

    chore: tokyonight

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "rebelot/kanagawa.nvim",
-  name = "kanagawa",
+  "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme kanagawa")
+    vim.cmd("colorscheme tokyonight-night")
   end,
 }
