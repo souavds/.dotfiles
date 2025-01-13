2b49ca32fcc542b6042ea02ecd24a0b1da2d7192
Arthur Diniz <arthurvdinizs@gmail.com>
2025-01-12 22:11:28 -0300

    chore(nvim): blink and copilot

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,19 +1,78 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "giuxtaposition/blink-cmp-copilot",
+    { "L3MON4D3/LuaSnip", version = "v2.*" },
+  },
   version = "*",
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     keymap = { preset = "default" },
     appearance = {
-      use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
+      kind_icons = {
+        Copilot = "",
+        Text = "󰉿",
+        Method = "󰊕",
+        Function = "󰊕",
+        Constructor = "󰒓",
+
+        Field = "󰜢",
+        Variable = "󰆦",
+        Property = "󰖷",
+
+        Class = "󱡠",
+        Interface = "󱡠",
+        Struct = "󱡠",
+        Module = "󰅩",
+
+        Unit = "󰪚",
+        Value = "󰦨",
+        Enum = "󰦨",
+        EnumMember = "󰦨",
+
+        Keyword = "󰻾",
+        Constant = "󰏿",
+
+        Snippet = "󱄽",
+        Color = "󰏘",
+        File = "󰈔",
+        Reference = "󰬲",
+        Folder = "󰉋",
+        Event = "󱐋",
+        Operator = "󰪚",
+        TypeParameter = "󰬛",
+      },
     },
     completion = {
       menu = { border = "rounded" },
       documentation = { window = { border = "rounded" } },
     },
     signature = { enabled = true, window = { border = "rounded" } },
+    snippets = {
+      preset = "luasnip",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+        },
+      },
+    },
   },
   opts_extend = { "sources.default" },
 }
